b85d583a29471ea60eea461407cdd83a992f487b
Bluedigi <edmvp98@gmail.com>
2025-05-13 16:45:43 -0600

    añadida animacion a seccion experience

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,10 +1,13 @@
-import SectionWrapper from '../components/sectionWrapper';
-import type { Experience } from '../types/type';
+import SectionWrapper from '../components/sectionWrapper'
+import type { Experience } from '../types/type'
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 
 const experiencias: Experience[] = [
   {
     id: 1,
-    position: 'Desarrollador Backend Jr.',
+    position: '1 Desarrollador Backend Jr.',
     company: 'Empresa Tech S.A.',
     description: 'Desarrollo y mantenimiento de microservicios con Java y Spring Boot.',
     startDate: '2023-03',
@@ -14,7 +17,37 @@ const experiencias: Experience[] = [
   },
   {
     id: 2,
-    position: 'Practicante en Desarrollo de Software',
+    position: '2 Practicante en Desarrollo de Software',
+    company: 'Startup ABC',
+    description: 'Apoyé en el desarrollo de componentes para el frontend y backend.',
+    startDate: '2022-06',
+    endDate: '2022-12',
+    location: 'Remoto',
+    technologies: ['JavaScript', 'React', 'Node.js'],
+  },
+  {
+    id: 3,
+    position: '3 Practicante en Desarrollo de Software',
+    company: 'Startup ABC',
+    description: 'Apoyé en el desarrollo de componentes para el frontend y backend.',
+    startDate: '2022-06',
+    endDate: '2022-12',
+    location: 'Remoto',
+    technologies: ['JavaScript', 'React', 'Node.js'],
+  },
+  {
+    id: 4,
+    position: '4 Practicante en Desarrollo de Software',
+    company: 'Startup ABC',
+    description: 'Apoyé en el desarrollo de componentes para el frontend y backend.',
+    startDate: '2022-06',
+    endDate: '2022-12',
+    location: 'Remoto',
+    technologies: ['JavaScript', 'React', 'Node.js'],
+  },
+  {
+    id: 5,
+    position: '5 Practicante en Desarrollo de Software',
     company: 'Startup ABC',
     description: 'Apoyé en el desarrollo de componentes para el frontend y backend.',
     startDate: '2022-06',
@@ -31,6 +64,13 @@ function formatFecha(fecha: string | undefined): string {
 }
 
 export default function Experience() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+    });
+  }, []);
   return (
     <SectionWrapper id="experiencia">
       <div className="w-full opacity-0 animate-fade-in-start">
@@ -39,6 +79,7 @@ export default function Experience() {
         <ul className="space-y-12 flex flex-col items-center">
           {experiencias.map((exp) => (
             <li
+              data-aos="fade-up"
               key={exp.id}
               className="bg-gray-50 shadow-md rounded-lg p-6 w-full max-w-2xl text-center"
             >
